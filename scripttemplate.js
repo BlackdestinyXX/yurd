8d3c30961e347c50c1872d3dddbe9bdec2d53317
BlackdestinyXX <65021823+BlackdestinyXX@users.noreply.github.com>
2021-12-31 10:53:36 +0100

    Update scripttemplate.js

--- scripttemplate.js
+++ scripttemplate.js
@@ -1,11 +1,13 @@
 const code = `code here`
 
 // const code2 = `code here`
+// const typescriptCode = `typescript code here`
 
 module.exports = {
-    code: [{fileName: 'file name', fileCode: code}, /*{fileName: 'file name 2', fileCode: code2}*/],
+    code: [{fileName: 'file name', fileCode: code, /*typescriptCode: typescriptCode*/}, /*{fileName: 'file name 2', fileCode: code2}*/],
     name: ['template name', 'template name2'],
     inizializeCommand: 'npm init -y',
     dependenciesCommand: 'npm install dep1 dep2',
+    typescriptDependiencesCommand: 'npm install @types/node',
     supportTypescript: true /*or false*/,
 }
